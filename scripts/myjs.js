bd07238b7e6694c052acb24adadd70210de753af
luoaiqun <tinhaichenfv@163.com>
2018-09-03 23:20:14 +0800

    commit

--- scripts/myjs.js
+++ scripts/myjs.js
@@ -302,6 +302,27 @@ document.addEventListener('DOMContentLoaded', function () {
     };
   }
 
+  window.oncontextmenu=function(e){
+    //取消默认的浏览器自带右键 很重要！！
+    e.preventDefault();
+
+    //获取我们自定义的右键菜单
+    var menu=document.querySelector("#menu");
+
+    //根据事件对象中鼠标点击的位置，进行定位
+    menu.style.left=e.clientX+'px';
+    menu.style.top=e.clientY+'px';
+
+    //改变自定义菜单的宽，让它显示出来
+    menu.style.width='125px';
+  }
+
+  //关闭右键菜单，很简单
+  window.onclick=function(e){
+    //用户触发click事件就可以关闭了，因为绑定在window上，按事件冒泡处理，不会影响菜单的功能
+　　document.querySelector('#menu').style.width=0;
+  }
+
   initGrid();
 });
 
